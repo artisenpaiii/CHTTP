39566ce9d401516df7b626d242ed6787ad689c80
ArtiSenpai <bryanr2002@icloud.com>
2025-08-07 00:43:04 +0200

    first commit

---
+++ include/http_server.h
@@ -0,0 +1,15 @@
+
+#ifndef HTTP_SERVER
+#define HTTP_SERVER
+
+#include <string>
+
+    class HTTP_Server{
+    public:
+        void start(std::string& const ip_adress, int const port_nr) const;
+
+    private:
+
+};
+
+#endif;
---
+++ src/http_server.cpp
@@ -0,0 +1,8 @@
+#include "http_server.h"
+
+void HTTP_Server::start(std::string& const ip_adress, int const port_nr) const {
+
+    
+    
+
+}
